2407653bad3e8e1ac6c6ebec453c3f148140e7c6
Walter Rizo <wrizo91@gmail.com>
2018-03-01 18:13:08 -0600

    address-assgnmnt-2

--- models/address_book.rb
+++ models/address_book.rb
@@ -18,6 +18,16 @@ class AddressBook
        index+= 1
      end
      # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
+     entries.insert(index, Entry.new(name, phone_number, email)
+   end
+
+   def remove_entry (name, phone_number, email)
+     entry_to_be_removed = nil
+     entries.each do |entry|
+       if name == entry.name && phone_number == entry.phone_number && email == entry.email
+         entry_to_be_removed = entry
+       end
+     end
+     entries.delete(entry_to_be_removed)
    end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -34,5 +34,21 @@ require_relative '../models/address_book'
        expect(new_entry.phone_number).to eq('010.012.1815')
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
-   end     
+   end
+
+   describe "#remove_entry" do
+     it "removes only one entry from the address book" do
+       book = AddressBook.new
+       book.add_entry("Walter Rizo", "123.123.4567", "walter.rizo@bloc.com")
+       name= "Ada Lovelace"
+       phone_number= "010.012.1815"
+       email= "augusta.king@lovelace.com"
+       book.add_entry(name, phone_number, email)
+
+       expect(book.entries.size).to eq 2
+       book.remove_entry(name, phone_number, email)
+       expect(book.entries.size).to eq 1
+       expect(book.entries.first.name).to eq("Walter Rizo")
+     end
+   end
  end
