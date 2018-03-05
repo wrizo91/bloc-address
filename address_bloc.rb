0311b8bd935e9822e255ddeb0e047c1d69d3c4d9
Walter Rizo <wrizo91@gmail.com>
2018-03-05 10:52:11 -0600

    chckpnt-3

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,10 @@
-puts "Welcom to AddressBloc!"
+require_relative 'controllers/menu_controller'
+
+ # #4
+ menu = MenuController.new
+ # #5
+ system "clear"
+ puts "Welcome to AddressBloc!"
+
+ # #6
+ menu.main_menu
