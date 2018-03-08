f944860412600eaa4a12b6e548f0152f153faf5d
Walter Rizo <wrizo91@gmail.com>
2018-03-07 18:51:07 -0600

    chkpnt-4

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,9 +1,31 @@
- require_relative 'controllers/menu_controller'
+require_relative 'entry'
+require "csv"
 
- # #4
- menu = MenuController.new
- # #5
- system "clear"
- puts "Welcome to AddressBloc!"
- # #6
- menu.main_menu
+ class AddressBook
+   attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1
+     end
+     entries.insert(index, Entry.new(name, phone_number, email))
+   end
+
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+   # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+   end
+ end
