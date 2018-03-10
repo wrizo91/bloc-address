f05d612e0cc295016e3e97cce53ef252dae16791
Walter Rizo <wrizo91@gmail.com>
2018-03-10 10:35:12 -0600

    chkpnt5

--- models/address_book.rb
+++ models/address_book.rb
@@ -28,4 +28,28 @@ require "csv"
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
    end
+
+   def binary_search(name)
+     lower = 0
+     upper = entries.length - 1
+
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     # #5
+     return nil
+   end
  end
